378d296f1a778089498b256d09911837d3ad5506
sun <1980987589@qq.com>
2025-11-05 21:24:15 -0800

    first commit

---
+++ src/main/java/com/safepath/SafePathApplication.java
@@ -0,0 +1,13 @@
+package com.safepath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafePathApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafePathApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/safepath/SafePathApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safepath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafePathApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
